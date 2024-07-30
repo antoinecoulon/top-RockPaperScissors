a1d06e4d2e87ac2989db92710001c186d02b04a5
Antoine COULON <a.antoinecoulon@gmail.com>
2024-07-30 09:39:51 +0200

    Uncompleted feature: fonction choix de l'IA

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello world!");
+let computerChoice;
+
+function getComputerChoice() {
+    randomNumber = Math.random();
+    console.log(randomNumber);
+
+    if (randomNumber <= 0,33) {
+        computerChoice = "Rock";
+    } else if (randomNumber > 0,33 && randomNumber <= 0,66) {
+        computerChoice = "Paper";
+    } else {
+        computerChoice = "Scissors";
+    }
+
+    return computerChoice;
+    console.log(computerChoice);
+
+    // Retourne toujours Rock !!!
+}
+
