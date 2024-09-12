456b73b7dc66c56b1e5ca3c1cc3e6cf4bd399b36
Antoine COULON <a.antoinecoulon@gmail.com>
2024-09-12 11:25:02 +0200

    Add function playRound

--- script.js
+++ script.js
@@ -6,13 +6,40 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-let computerChoice = getComputerChoice();
-console.log(computerChoice);
-
 function getHumanChoice() {
-    let humanChoice = prompt();
+    let humanInput = prompt();
+    let humanChoice = humanInput.toLowerCase();
     return humanChoice;
 }
 
-let humanChoice = getHumanChoice();
-console.log(humanChoice);
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    const outcomes = {
+        rock: 'scissors',
+        scissors: 'paper',
+        paper: 'rock'
+    };
+    
+    if (humanChoice === computerChoice) {
+        return "It's a tie!";
+    } else if (outcomes[humanChoice] === computerChoice) {
+        humanScore++;
+        return "You win!";
+    } else {
+        computerScore++;
+        return "Computer wins!";
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(humanSelection);
+console.log(computerSelection);
+
+let result = playRound(humanSelection, computerSelection);
+console.log(result);
+console.log("Human score is " + humanScore + " and Computer score is " + computerScore);
+
